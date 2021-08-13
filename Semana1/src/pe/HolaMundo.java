00193754b35d7df62f81e3b55b853cdd17e79bbc
steampunkyi <u20219834@utp.edu.pe>
2021-08-12 21:45:40 -0500

    CLase inicio

---
+++ Semana1/src/pe/HolaMundo.java
@@ -0,0 +1,15 @@
+package pe.utp;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        //Comentario en una linea
+        /*
+         * comentarios
+         * en
+         * varias
+         * lineas
+         */
+        System.out.println("Hola Mundo");
+    }
+}

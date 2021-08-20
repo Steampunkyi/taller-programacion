f84c31c895f872d21bf18b4dccb5481b0c9dc9f1
steampunkyi <u20219834@utp.edu.pe>
2021-08-19 19:49:08 -0500

    Cambio en identificadores

--- Semana02/src/peUTP/Identificadores.java
+++ Semana02/src/peUTP/Identificadores.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package peUTP;
 
 public class Identificadores {
@@ -19,25 +18,3 @@ public class Identificadores {
         System.out.println(Producto);
     }
 }
-=======
-package peUTP;
-
-public class Identificadores {
-
-    public static void main(String[] args) {
-        // Los identificadores deben estar en forma de letras y/o números
-        int edad;
-        double precio2;
-        String $nombre;
-        char _categoria;
-        // No puede empezar con un número
-        // int 2edad;
-        // Java es case sensitive: Sensible a mayúsculas y minúsculas
-        String Producto = "Televisor";
-        String producto = "Equipo de sonido";
-        String PRODUCTO = "SmartPhone";
-
-        System.out.println(Producto);
-    }
-}
->>>>>>> 5bb955424e91ecab8f9b73b7ab9b842ac848a213

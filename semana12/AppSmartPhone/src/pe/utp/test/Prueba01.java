50da7950feea4ffca83116acb38c6a69ae4e5b9b
steampunkyi <u20219834@utp.edu.pe>
2021-11-11 17:37:51 -0500

    Faltaba }

--- semana12/AppSmartPhone/src/pe/utp/test/Prueba01.java
+++ semana12/AppSmartPhone/src/pe/utp/test/Prueba01.java
@@ -7,11 +7,11 @@ import javax.swing.plaf.synth.SynthSpinnerUI;
 import pe.utp.model.Smartphone;
 
 public class Prueba01 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner Leer = new Scanner(System.in);
         System.out.println("Ingrese el numero de telefono");
         int nro = Leer.nextInt();
-        for (int i=0; i<nro; i++){
+        for (int i = 0; i < nro; i++) {
             System.out.println("Ingrese la marca del telefono");
             String marca = Leer.next();
             System.out.println("Ingrese el modelo de telefono");
@@ -27,8 +27,9 @@ public class Prueba01 {
             // Hacer uso del metodo set
             cel1.setModelo("P40 Pro");
             // Hacer uso del metodo get
-            System.out.println("Marca: "+cel1.getMarca());
-            System.out.println("Modelo: "+cel1.getModelo());
+            System.out.println("Marca: " + cel1.getMarca());
+            System.out.println("Modelo: " + cel1.getModelo());
             System.out.println("==================================");
+        }
     }
 }
